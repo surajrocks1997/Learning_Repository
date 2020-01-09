2ac2c76cab85e9bb7b08890730f9612d7a6426b0
surajrocks1997 <surajrocks1997@gmail.com>
2020-01-09 20:38:14 +0530

    Spring Boot WebApp Learning Repo

---
+++ Spring_Boot_WebApp_Demo/src/main/java/com/spring/Spring_Boot_WebApp_Demo/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.spring.Spring_Boot_WebApp_Demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")
+	public String home() {
+		return "Home";
+	}
+}
---
+++ Spring_Boot_WebApp_Demo/src/test/java/com/spring/Spring_Boot_WebApp_Demo/SpringBootWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.Spring_Boot_WebApp_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
