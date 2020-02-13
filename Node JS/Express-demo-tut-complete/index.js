c85d61d8b8c2dc1321f7428f8b89b4238d7d390c
surajrocks1997 <surajrocks1997@gmail.com>
2020-02-13 16:41:14 +0530

    following standard practice

--- Node JS/Express-demo-tut-complete/index.js
+++ Node JS/Express-demo-tut-complete/index.js
@@ -1,80 +1,26 @@
-const logger = require('./logger');
-const authenticator = require('./authenticator');
-
+const config = require('config');
 const express = require('express');
 const {check, validationResult} = require('express-validator');
+
+const home = require('./routes/home');
+const courses = require('./routes/courses');
+const logger = require('./middleware/logger');
+const authenticator = require('./middleware/authenticator');
+
+
 const app = express();
 
-app.use(express.json());
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use('/', home);
+app.use('/api/courses', courses);
 
-// app.use(logger);
-// app.use(authenticator);
-
-const courses = [
-    { id: 1, name: 'course1'},
-    { id: 2, name: 'course2'},
-    { id: 3, name: 'course3'},
-    { id: 4, name: 'course4'}
-]
-
-app.get('/', (req, res) => {
-    res.send('Hello world!!');
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if(!course){
-        return res.status(404).send('Course with given id not found');
-    }
-    res.send(course);
-});
-
-app.post('/api/courses', [check('name').isLength({min: 3}).not().isEmpty()], (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status(422).jsonp(errors.array());
-    
-    const course = {
-        id: courses.length + 1, 
-        name: req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-    
-});
-
-app.put('/api/courses/:id', [check('name').isLength({min: 3}).not().isEmpty()], (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    
-    if(!course){
-        return res.status(404).send('Course with given id not found');
-    }
-
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status(422).jsonp(errors.array());
-    
-    course.name = req.body.name;
-    res.send(course);
-
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    
-    if(!course){
-        return res.status(404).send('Course with given id not found');
-    };
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-
-});
+app.use(logger);
+app.use(authenticator);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
---
+++ Node JS/Express-demo-tut-complete/routes/courses.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+    { id: 4, name: 'course4'}
+];
+
+router.get('/', (req, res) => {
+    res.send(courses);
+});
+
+router.get('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course){
+        return res.status(404).send('Course with given id not found');
+    }
+    res.send(course);
+});
+
+router.post('/', [check('name').isLength({min: 3}).not().isEmpty()], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(422).jsonp(errors.array());
+    
+    const course = {
+        id: courses.length + 1, 
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+    
+});
+
+router.put('/:id', [check('name').isLength({min: 3}).not().isEmpty()], (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    
+    if(!course){
+        return res.status(404).send('Course with given id not found');
+    }
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(422).jsonp(errors.array());
+    
+    course.name = req.body.name;
+    res.send(course);
+
+});
+
+router.delete('/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    
+    if(!course){
+        return res.status(404).send('Course with given id not found');
+    };
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+
+});
+
+module.exports = router;
