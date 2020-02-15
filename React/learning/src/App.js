6304ed4fe1378d921d5abe081b1a6c1c9b68a97f
surajrocks1997 <surajrocks1997@gmail.com>
2020-02-16 00:38:44 +0530

    git ignore file added

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import './App.css';
-
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
-  render(){
-    return (
-      <div className="App">
-        <Navbar title='Vinder' icon='fab fa-github'/>
-		<div className="container">
-			<Users/>
-		</div>
-      </div>
-    );
-  }
+	state = {
+		users: [],
+		loading: false
+	}
+
+	async componentDidMount(){
+		this.setState({ loading: true});
+		const res = await axios.get('https://api.github.com/users');
+		this.setState({ users: res.data, loading: false});
+	}
+    render() {
+        return (
+            <div className="App">
+                <Navbar title="Vinder" icon="fab fa-github" />
+                <div className="container">
+                    <Users loading={this.state.loading} users={this.state.users}/>
+                </div>
+            </div>
+        );
+    }
 }
 export default App;
