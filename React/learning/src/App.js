027bd4118fd57836ca0934ff82f44823f382b5c5
surajrocks1997 <surajrocks1997@gmail.com>
2020-02-15 23:48:03 +0530

    Learning React - Front to Back

---
+++ React/learning/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import './App.css';
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Navbar title='Vinder' icon='fab fa-github'/>
+		<div className="container">
+			<Users/>
+		</div>
+      </div>
+    );
+  }
+}
+export default App;
