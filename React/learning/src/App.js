8b484d5b7ce2b5d34d79be1c3587812fe4d46084
surajrocks1997 <surajrocks1997@gmail.com>
2020-02-16 22:39:14 +0530

    React Router added. working

--- React/learning/src/App.js
+++ React/learning/src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from './components/pages/About';
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-	state = {
-		users: [],
-		loading: false
-	}
+    state = {
+        users: [],
+        loading: false,
+        alert: null
+    };
 
-	async componentDidMount(){
-		this.setState({ loading: true});
-		const res = await axios.get('https://api.github.com/users');
-		this.setState({ users: res.data, loading: false});
-	}
+    // async componentDidMount() {
+    //     this.setState({ loading: true });
+    //     const res = await axios.get(
+    //         `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    //     );
+    //     this.setState({ users: res.data, loading: false });
+    // }
+    searchUsers = async text => {
+        this.setState({ loading: true });
+        const res = await axios.get(
+            `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
+        this.setState({ users: res.data.items, loading: false });
+    };
+
+    clearUsers = () => {
+        this.setState({ users: [], loading: false });
+    };
+
+    setAlert = (msg, type) => {
+        this.setState({ alert: { msg: msg, type: type } });
+        setTimeout(() => {
+            this.setState({ alert: null });
+        }, 5000);
+    };
     render() {
         return (
-            <div className="App">
-                <Navbar title="Vinder" icon="fab fa-github" />
-                <div className="container">
-                    <Users loading={this.state.loading} users={this.state.users}/>
+            <Router>
+                <div className="App">
+                    <Navbar title="Vinder" icon="fab fa-github" />
+                    <div className="container">
+                        <Alert alert={this.state.alert} />
+                        <Switch>
+                            <Route exact path='/' render={props => (
+                                <Fragment>
+                                    <Search
+                                        searchUsers={this.searchUsers}
+                                        clearUsers={this.clearUsers}
+                                        showClear={
+                                            this.state.users.length > 0 ? true : false
+                                        }
+                                        setAlert={this.setAlert}
+                                    />
+                                    <Users
+                                        loading={this.state.loading}
+                                        users={this.state.users}
+                                    />
+                                </Fragment>
+                            ) }/>
+                            <Route exact path='/about' component={About} />
+                        </Switch>
+                    </div>
                 </div>
-            </div>
+            </Router>
         );
     }
 }
--- React/learning/src/components/layout/Navbar.js
+++ React/learning/src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({icon, title}) => {
     return (
@@ -8,11 +9,19 @@ const Navbar = ({icon, title}) => {
                 <i className={icon} />
                 {title}
             </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
         </nav>
     );
 };
 Navbar.defaultProps = {
-    title: "Github Finder",
+    title: "Github Vinder",
     icon: "fab fa-github"
 };
 Navbar.propTypes = {
